474b3dd29a91dbb7561fb2ca0d283e0b261adfa7
sanjay <sanjaytomar717@gmail.com>
2024-02-22 16:59:12 +0530

    first commit

---
+++ src/pages/Events.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateEvent from "../components/CreateEvent";
+
+const Events = () => {
+  return (
+    <div>
+      <CreateEvent />
+    </div>
+  );
+};
+
+export default Events;
